56719bd32d604ca4eea2536b8e51bce85ffb4ba2
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 19:36:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 def line(deliline)
   current_line = "The line is currently"
   if deliline.empty? == true
-    puts "The line is currently empty."
+    puts "#{current_line} empty."
   else
+    current_line.concat(":")
     deliline.each_with_index {|name, index|
-    deliline[index-1] = " #{index}. #{name}"}
+    current_line.concat(" #{index}. #{name}")}
+    puts current_line
   end
 end
       
