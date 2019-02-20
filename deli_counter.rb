e07d12fdf70deca2a8d8ad79e5f9d5d4127f4fa3
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 19:33:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 def line(deliline)
-  
-  if deliline.empty?
+  current_line = "The line is currently"
+  if deliline.empty? == true
     puts "The line is currently empty."
   else
     deliline.each_with_index {|name, index|
     deliline[index-1] = " #{index}. #{name}"}
   end
-  puts "The line is currently:#{deliline}"
 end
       
