eb67fd8a5f400cbfbd4bf3b3def2d99b8da6855d
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 20:11:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(deliline, name)
   deliline.push(name)
-  puts "Welcome, #{name}. You are number #{line.size+1} in line."
+  puts "Welcome, #{name}. You are number #{deliline.size+1} in line."
 end
